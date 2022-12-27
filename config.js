4961b000d6a40dab29a8235b716a92da5cc5cfef
luongthao2711 <114208722+luongthao2711@users.noreply.github.com>
2022-12-27 12:24:36 +0700

    meseage

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Khum có gì đâu",
+    introTitle: 'Đỗ Lương Trúc',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Ấn yêu cái nà^^',
+    title: ' Anh yêu chị! 🥰',
+    desc: 'chị làm người yêu của anh nhaaa <3 ',
+    btnYes: 'Chị đồng ý <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tui <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'yêu thì yêu khum yêu thì yêu <3',
+    mess: 'Anh biết mà 🥰. Anh yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Tối nay, mình đi hẹn hò nhaaaa. Inbox để trao gửi yêu thương nà',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/thaodoluong0112?mibextid=ZbWKwL' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
